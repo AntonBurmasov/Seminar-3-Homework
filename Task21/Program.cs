f16a51615d8ae9ffb1e5f2c827680f48e8f40009
burmasovanton <looseangel1@gmail.com>
2022-09-09 13:11:09 +0500

    Working task21

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -6,3 +6,26 @@
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+Console.WriteLine("Введите координату xa:");
+double xa = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату ya:");
+double ya = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату za:");
+double za = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату xb:");
+double xb = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату yb:");
+double yb = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату zb:");
+double zb = Convert.ToInt32(Console.ReadLine());
+
+double AB = Math.Sqrt(Math.Pow(xb-xa, 2) + Math.Pow(yb-ya, 2) + Math.Pow(zb-za, 2));
+
+Console.Write("Расстояние между точками A и B равно ");
+Console.WriteLine($"{AB:f2}");
+
